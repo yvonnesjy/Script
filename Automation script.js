4c46af5b6a40ea2f774be64ac12b7b5729f62cc0
yvonnesjy <stonfish@hotmail.com>
2018-11-18 15:58:24 -0800

    Add files via upload

---
+++ Automation script.js
@@ -0,0 +1,155 @@
+Option+Command+i to bring up the developer console
+
+<option selected="selected" value="{a98eddf0-cdd6-48c8-938b-6f70ec3ffc5f}">0300-CMBH-1001 Conduct observation </option>
+<option value="{9baafdbd-7c47-4ef5-a759-f129304a2f30}">0300-COMM-1001 Communicate using hand and arm signals </option>
+<option value="{3457a311-321b-4ecf-8d36-975671a893ff}">0300-PAT-1009 Perform immediate actions </option>
+<option value="{ac8c2158-e550-4114-a979-293e2703f345}">AA&amp;E Screening - Use of Deadly Force Training</option>
+<option value="{51165c19-95c9-4753-861f-6c93ff505926}">AA&amp;E Screenings - Ammo &amp; Explosives Screenings</option>
+<option value="{637a50ba-d9a3-4583-bd36-5a35a20b3124}">AA&amp;E Screenings - Arms, Ammo &amp; Explosive Screened</option>
+<option value="{a3ca9c50-9ecc-4da3-b88b-f27258b71eb5}">Annual arms ammo and explosives screening</option>
+<option value="{cb1e891f-f9c5-43be-a56e-41c8892d9f4c}">arms/ammo and explosives screened</option>
+<option value="{09c469a2-7aec-404b-b613-210fbfc75dc4}">Blended Retirement System (BRS) Opt-in Training</option>
+<option value="{87b07d7f-dc79-4e6c-9afa-5df834b3a2d6}">BRS Election Training (JKO Certificate)</option>
+<option value="{56328713-6e01-4403-9607-0dabe154f2d9}">BRS Signed NAVMC 118(11) Event Code Z7</option>
+<option value="{def82a8c-503a-4412-bd45-54dde2e75829}">CACO Training</option>
+<option value="{d95fce95-1dc7-4e2e-9c88-cd87d92c2736}">CBRN Center Team Training </option>
+<option value="{e312a604-eb09-440e-8702-6b0046ae539c}">CBRN Decontamination (CBRN Decon) Team</option>
+<option value="{8e54aa79-699c-45c5-8a0f-42c431101ffb}">CBRN Hazardous Material Operation</option>
+<option value="{c819814e-ed18-4be5-a77a-d37642009ae4}">CBRN Hazardous Material Technician</option>
+<option value="{6b9ba9f0-e6b5-442f-bf0e-e7f231047eec}">CBRN Individual Training Standards (ITS)</option>
+<option value="{5666b26a-e187-4b69-a82d-9ed693955708}">CBRN Reconnaissance and Surveillance (R&amp;S) Team</option>
+<option value="{628ccc62-87ab-4444-8037-8917ebd38637}">Command-Sponsored Corporals Course</option>
+<option value="{6915d899-78e5-44ae-a875-1324ce25e765}">Command-Sponsored Lance Corporals Leadership and Ethics Seminar</option>
+<option value="{2bc268a1-8133-4d33-8ad6-2f50d6f20ce2}">Corporals Course</option>
+<option value="{ccaab307-722e-4ab0-8f81-b230b4151b7a}">CY - Behavioral Health Alcohol Screening Program</option>
+<option value="{bd0ded98-bbb4-4686-be62-89426880a8c4}">CY - Counterespionage/Insider Threat</option>
+<option value="{f1890cbe-43c2-4a80-8dc3-1d21aa813283}">CY - Hazing</option>
+<option value="{cb969f41-a57c-465c-9d41-643790189a39}">CY - Level I AT Awareness Training/Counter Intelligence Awareness</option>
+<option value="{1d9e7062-9bf5-4f65-9cc8-77b4b75346bb}">CY - Marine Corps Equal Opportunity (EO)</option>
+<option value="{38c1ae73-fd54-43a0-8256-774d1862699b}">CY - Prohibited Activities and Conduct Prevention and Response</option>
+<option value="{2518fdf3-0679-4cef-8bf7-f4cd5fef6705}">CY - Records Management Training</option>
+<option value="{e0eb41d4-8b02-415f-9c6a-e40639610799}">CY - Risk Management (RM)</option>
+<option value="{ed9723ef-ce19-4c34-bb3b-ecb9f6a82d90}">CY - Sexual Harassment</option>
+<option value="{b8a80010-9be7-4c8a-bf69-acf356317986}">CY - Supervisor Safety Training (Marine Corps Safety Program)</option>
+<option value="{d09f8a32-b3cc-456b-81e2-f93de17e0849}">CY - Supervisory level substance misuse prevention training</option>
+<option value="{d9b5ad0d-7078-46de-bc38-564da18e12d4}">CY - The Effects of Tobacco Use (Semper Fit)</option>
+<option value="{6680b4cf-cd68-4bd8-9179-a5a0b71a3a43}">CY - The Marine Corps Operations Security (OPSEC) Program</option>
+<option value="{e688d975-4f69-4568-8c27-5550b90342a1}">CY - Unit Marine Awareness and Prevention Integrated Training (UMAPIT 2.0)</option>
+<option value="{dd0e6d36-27f8-43c9-b965-ff51574ac2a0}">CY - Violence Prevention Awareness Course</option>
+<option value="{7f9fd77f-32bd-4b55-abd1-7298b00df4e4}">CY - Violence Prevention Recognition Course</option>
+<option value="{03ebac1d-0631-42ca-bd00-ec825c8c879f}">Driver improvement program</option>
+<option value="{cd222935-c574-4913-b3de-44ba23423671}">Driver's Awareness (Alive at 25, Marine Corps Traffic Safety Program)</option>
+<option value="{4061dcc5-746b-4656-aeb4-b6afd36ff5d8}">Ergonomics</option>
+<option value="{916e7152-8d0a-4ad3-9a2c-ed0dded74a5c}">Fire Safety</option>
+<option value="{ed9dcfb8-5ca3-49ff-b742-44d34204cf27}">FY - Annual Sexual Assault Prevention and Response (SAPR) Training</option>
+<option value="{2a6807dd-4ea5-4f89-a5bd-850c74044eb8}">FY - CBRN Defense Training Requirements</option>
+<option value="{f3726567-0a73-498e-8eb9-f440eb5de2d9}">FY - Combating Trafficking in Persons (CTIP)</option>
+<option value="{220ceb15-eda5-4888-a0b3-6eab3a1447f4}">FY - Cyber Awareness/Information Assurance</option>
+<option value="{969e40c7-ca12-4784-9a47-2a8d8bc19dc1}">FY - Marine Corps Combat Marksmanship - Rifle</option>
+<option value="{eba6faae-a06f-418a-ac47-6b268b594c81}">FY - Marine Corps Combat Marksmanship Rifle - Table 3 (Unknown Distance Day)</option>
+<option value="{9f6c07b5-474e-4c87-9e65-dff0e4a810ca}">FY - Marine Corps Combat Marksmanship Rifle - Table 4 (Unknown Distance Night)</option>
+<option value="{56464e00-f53d-461c-b5cf-6acbdd3d1ea1}">FY - Marine Corps Combat Marksmanship Rifle - Table 5 (Short Range Day)</option>
+<option value="{05fd4dae-f466-4a77-82f4-a7f97d683eca}">FY - Marine Corps Combat Marksmanship Rifle - Table 6 (Short Range Night)</option>
+<option value="{1605eba2-b083-491d-adc0-66709e782ddd}">FY - Marine Corps Combat Marksmanship-Pistol</option>
+<option value="{4a10bfeb-5125-4f22-8ef8-9a71dec1ddcc}">FY - Social Media Misconduct</option>
+<option value="{c7e025f1-22ed-4fa7-896e-adee90ee7f8c}">FY - Step Up (SAPR Training for E1-E3)</option>
+<option value="{85ccaf6f-4e70-4f17-a27d-f9ef01934438}">Government Travel Charge Card</option>
+<option value="{757ae956-4c5d-4234-b8b4-e72a2a402bff}">HAZCOM</option>
+<option value="{2b53b36b-0076-45ff-9b82-fa54d61ca253}">Hearing Conservation (HCP)</option>
+<option value="{5e350372-5006-4094-a18e-02c2a53b36e8}">Helo Egress Training (FAIL)</option>
+<option value="{8108a09a-9947-48d2-b825-84760164117c}">Helo Egress Training (QUAL)</option>
+<option value="{ebd8d49a-61cf-4f6d-9120-f3d63bf64e42}">Helo Egress Training (UNQUAL)</option>
+<option value="{9a641d44-f636-42c6-a46d-af62895c5a6a}">Laser Safety</option>
+<option value="{bd5d52f8-1fe7-4e6b-b35a-7187404c0a89}">MAGTF CBRN CM Team Training</option>
+<option value="{49c2895b-d278-433c-95e1-d8fa14218532}">Marine Corps Amphibious Egress Training (QUAL)</option>
+<option value="{223223dc-fdad-416a-900b-a9b7a97c420d}">Marine Corps Amphibious Egress Training (UNQUAL)</option>
+<option value="{d715f42a-701d-4a61-8bf0-ee52455df693}">Marine Corps Combat Fitness Test (CFT)</option>
+<option value="{5be9810d-1d0a-441a-b9c3-9adc10808076}">Marine Corps Martial Arts Program (MCMAP)</option>
+<option value="{3064877d-f8af-46f6-aad9-9c157ebce4b6}">Marine Corps Physical Fitness Test (PFT)</option>
+<option value="{1a8eede0-95f6-46d0-8b6f-0fce63d7cbd2}">Marine Corps Water Survival Training (MCWST)</option>
+<option value="{20e7bed9-4a6d-499e-a362-5276fadbcb1f}">Marksmanship Shotgun Range </option>
+<option value="{3ff87e3a-c675-49ae-b16e-ebacb206f81b}">May ASP Screening</option>
+<option value="{306daa8c-67d1-4149-9056-f8326ef4e72e}">MCRD-ATFP-1002 Describe the use of deadly force</option>
+<option value="{4299a84f-3785-4827-bdb2-2c70e6cd2d03}">MCRD-ATFP-1003 Stand a sentry post</option>
+<option value="{656192b4-a73f-4e4d-b129-326d64ad5789}">MCRD-HIST-1002 Identify significant events in Marine Corps history</option>
+<option value="{902055dc-f955-47c2-adce-2cc11be5f1c1}">MCRD-HIST-1003 Identify the historical significance of Marine Corps uniform items</option>
+<option value="{cc1cff05-07cf-4b2a-bee7-2af064f506df}">MCRD-LDR-1014 Describe Operational Security (OPSEC)</option>
+<option value="{d72bb9fa-af1e-4b2e-9a58-d35d1daf5929}">MCRD-LDR-1017 Describe stresses of combat</option>
+<option value="{43a26dc4-f6d0-4b60-9e52-b9f7459f7d40}">MCRD-LDR-1021 Describe the Code of Conduct</option>
+<option value="{db1040a4-d98b-480e-a0d5-5aaea857bdb0}">MCRD-LDR-1022 Describe your rights as a Prisoner of War (POW)</option>
+<option value="{7eea84f0-2648-4f9e-b02e-7dc3ab315d71}">MCRD-M16-1001 Perform weapons handling procedures with a service rifle</option>
+<option value="{fc25966e-bbb1-42fe-b5d3-2aecd0de8f2e}">MCRD-M16-1003 Perform corrective action with a service rifle</option>
+<option value="{abf729d8-78fa-48dc-9aec-27221d09f8fe}">MCRD-MED-1001 Describe phases of Tactical Combat Casualty Care (TCCC)</option>
+<option value="{eed0c7a4-0d91-4a5c-b740-ff95a8cd8cd7}">MCRD-MED-1005 Apply a tourniquet</option>
+<option value="{cdce6885-8f63-42ab-8071-cdd9c59cc5cf}">MCRD-MED-1013 Treat a heat injury</option>
+<option value="{864b3760-984a-4525-96c4-7b3e1d8ac946}">MCRD-MED-1014 Treat a cold injury </option>
+<option value="{52bbbd44-3e62-43a8-ba7e-59308f2aabd7}">MCRD-MGTF-1003 Describe Marine Air-Ground Task Force (MAGTF) organizations</option>
+<option value="{0b65d699-d538-4ec8-87cc-dc9e7e5235a8}">MCRD-PAT-1002 Navigate with a map and compass </option>
+<option value="{3c2a5dbd-0fb1-42ed-9c12-334d5cb47c3c}">MCRD-UCMJ-1001 Describe Article 15, Non-Judicial Punishment (NJP) </option>
+<option value="{e73382f3-9fe9-4b66-ac82-80c65db717f6}">MCRD-UCMJ-1002 Describe Article 31, Rights of the Accused </option>
+<option value="{f1f9eb48-655e-4246-b3e5-ea359b8d2273}">MCT-CMBH-1002 Identify anomalies</option>
+<option value="{ae22c5d0-4dd1-4399-91cc-432c0fbd49f0}">MCT-CMBH-1004 Apply the components of the decision cycle (OODA) process</option>
+<option value="{11801203-47e4-449f-a3b9-2c5df2f25bba}">MCT-COMM-1003 Operate a VHF field radio </option>
+<option value="{2169cec9-3218-4179-b749-597c54a1e110}">MCT-COMM-1004 Submit a message </option>
+<option value="{5544150b-a831-4498-b1e4-476531c5a619}">MCT-DEF-1002 Defend a position</option>
+<option value="{968f0aea-ffda-4488-b29b-4914a31718eb}">MCT-IED-1001 Visually identify indicators of improvised explosive devices (IED)</option>
+<option value="{d6dd63eb-77a8-49e9-b513-a55aeef15164}">MCT-PAT-1001 Prepare for combat</option>
+<option value="{9e05c5c1-dbed-4fa0-93ed-5157ab1726e3}">MCT-PAT-1004 Handle detainees</option>
+<option value="{b5b45b65-1ae3-4262-b2af-9bac007ea347}">NIGER OFFICERS GUIDED DISCUSSION </option>
+<option value="{ae9ab34c-ab09-41d0-8106-e9d999a75091}">Off-Duty Recreational Safety</option>
+<option value="{c92c6006-b024-4884-8466-bf4753deba42}">Op Pause </option>
+<option value="{90b443fd-5ed1-4691-bd4e-923ff8fa6be8}">OSCAR Master Trainer </option>
+<option value="{9e123330-a738-408b-afb0-f698c2cd2852}">OSCAR Team Member</option>
+<option value="{be8ed1b3-813a-4511-94dc-f43bf129a057}">OSCAR Team Member - Advanced </option>
+<option value="{01c1f360-2070-4772-a06c-4832a0d945c6}">OSCAR Trainer</option>
+<option value="{d8383dcc-20f4-4280-ac47-ad328e83309c}">Personal Readiness Seminar (PR) </option>
+<option value="{4874af6f-5ae6-4dd6-9c94-9c470b144b68}">PTP - AFRICOM Theater Specific C-IED Threat and Awareness Brief</option>
+<option value="{4999f811-910a-46ac-ac71-aca6e3bd2f74}">PTP - CENTCOM Theater Specific C-IED Threat and Awareness Brief</option>
+<option value="{0f8bfc48-2bd7-422e-90e1-75decf284b48}">PTP - Combat and Operational Stress Control (COSC) Program (Pre-Deployment E-1 through E-5)</option>
+<option value="{2f5d9811-33e4-4a59-9110-8e587c95468f}">PTP - Combat and Operations Stress Control (COSC) Program</option>
+<option value="{b6320e03-d9f8-4fc5-86fa-87b33142851a}">PTP - Combat and Operations Stress Control (COSC) Program  (Post Deployment E-1 through E-5)</option>
+<option value="{8b80e61c-e8fa-4df2-9ef1-a672d04b0098}">PTP - Combat and Operations Stress Control (COSC) Program  (Post Deployment E-6 and above)</option>
+<option value="{39a14d64-733c-46cb-b385-9f6310b28e40}">PTP - Combat and Operations Stress Control (COSC) Program  (Pre-Deployment E-6 and above)</option>
+<option value="{35b24b05-6a2c-4657-9acd-a96f74987f69}">PTP - Combat and Operations Stress Control (COSC) Program  (Returning E-1 through E-5)</option>
+<option value="{a55c1a8f-eea0-45b7-b0b3-9cd9ef78d317}">PTP - Combat and Operations Stress Control (COSC) Program  (Returning E-6 and above)</option>
+<option value="{660b1760-f0b4-4005-a388-897b2f9663da}">PTP - Combat Life Saver (CLS)</option>
+<option value="{02716ddd-071e-47be-a767-3812dad0e927}">PTP - EUCOM Theater Specific C-IED Threat and Awareness Brief</option>
+<option value="{a776b508-da08-42a2-9db9-365e1a6af855}">PTP - Law of War</option>
+<option value="{dbcd3340-707c-46d9-8eb3-7103bc56ba51}">PTP - Operational Culture Training</option>
+<option value="{43732848-3e7e-426e-b0c7-b8b998af29b8}">PTP - Operational Language Training</option>
+<option value="{012c587a-3117-4bd9-bd1c-57d81da58752}">PTP - PACOM Theater Specific C-IED Threat and Awareness Brief</option>
+<option value="{e234d6e4-046c-4fe1-8ae3-e2dae2bc041c}">PTP - Personnel Recovery (PR) Training (ISOPREP)</option>
+<option value="{3dac2644-ff1b-4d3c-a36d-11ad34132961}">PTP - Personnel Recovery (PR) Training (SERE)</option>
+<option value="{0e95aa42-0913-418c-8b3e-a7d0dc983cfd}">PTP - SOUTHCOM Theater Specific C-IED Threat and Awareness Brief</option>
+<option value="{741950ac-5729-426f-8fa8-04854f9ea638}">PTP - Tactical Combat Casualty Care (TCCC)</option>
+<option value="{515b4056-da40-4bfb-bfaa-304ab41effae}">PTP - Traumatic Brain Injury (TBI) Program </option>
+<option value="{186b07aa-384d-40de-813e-36ec64feeb98}">Radiation Safety</option>
+<option value="{7560c636-ca61-4e67-b51f-f16cf821084d}">Semi-Annual Height Weight Body Fat </option>
+<option value="{ab52bee5-18cb-4d32-a9a6-9b4a64f6092f}">Sergeants Course</option>
+<option value="{716faf80-13af-4186-abf2-44ee7c929acb}">Shallow Water Egress Training (QUAL)</option>
+<option value="{0f579b2a-1f75-4696-a060-56cb4862c354}">Shallow Water Egress Training (UNQUAL)</option>
+<option value="{ca193c83-763a-4b02-96ac-d2f601ad9ca6}">Social Media Guided Discussion</option>
+<option value="{4d6eeef0-23f9-43e9-9b61-f16a5721c8bf}">Suicide Awareness</option>
+<option value="{47825d41-0d98-43cb-94ae-7376e2094317}">TACTICAL COMBAT CASUALTY CARE - CLS (Combat Life Saver)</option>
+<option value="{5701f02e-fb4a-482c-a1d0-17ccd65ab662}">TACTICAL COMBAT CASUALTY CARE - CM/M (Combat Medic / Medical Corpsman)</option>
+<option value="{900f1e44-105a-4d2f-913c-2bf3ab1139a4}">Take A Stand</option>
+<option value="{4f8c9492-5f6f-4a02-978a-9668987cf863}">Total Force Integration Training</option>
+<option value="{501e1fe1-b958-4722-a4c5-ce8db94c8769}">Transition Readiness Capstone (TZ) </option>
+<option value="{675c61f8-808a-4663-aae8-ad321bcd2019}">Transition Readiness Seminar (TA) </option>
+<option value="{f0c8e0bc-ef72-4b40-b718-68f194abb123}">UMAPIT Master Trainer</option>
+<option value="{cd748ef5-2014-48a6-ae9c-7517fbc93f07}">UMAPIT Trainer</option>
+<option value="{751020fc-ee85-4c34-bb2b-6b275f2df7f8}">Unauthorized Disclosure</option>
+<option value="{bfb4b3c0-482c-43fb-894d-75fad2bd025d}">Urinalysis Testing Program</option>
+<option value="{37ad3105-d3ca-46f0-9e8e-3eceed1e46c2}">Use of deadly force training</option>
+
+$("#ddlRequirement").val("{cb969f41-a57c-465c-9d41-643790189a39}").change();
+$("#ctl00_ctl00_MainContent_mainContent_txtStartDate_date").val("20180101").change();
+$("#ctl00_ctl00_MainContent_mainContent_txtEndDate_date").val("20181008").change();
+$("#ctl00_ctl00_MainContent_mainContent_btnRunReport").click();
+
+$("#ctl00_ctl00_MainContent_mainContent_gridViewHelper_btnCSVExportButton_grdView_CD").click();
+
+$("#ddlRequirement").val("").change();
+$("#ctl00_ctl00_MainContent_mainContent_txtStartDate_date").val("20171008").change();
+$("#ctl00_ctl00_MainContent_mainContent_txtEndDate_date").val("20181008").change();
+$("#ctl00_ctl00_MainContent_mainContent_btnRunReport").click();
